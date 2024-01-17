9824d7cd27c5ac767898dabfe75b31581e3fcb4d
agulujan41 <agulujan41proj@gmail.com>
2024-01-17 20:54:50 -0300

    git ending 0

---
+++ public/script.js
@@ -0,0 +1,143 @@
+let cant = 35;
+if (window.innerWidth > 800) {
+  cant = 100;
+}
+for (let i = 0; i < cant; i++) {
+  let star = document.createElement("div");
+  star.classList.add("estrella");
+  var randomWidth = Math.floor(Math.random() * (50 - 5 + 1)) + 50; // Tamaño aleatorio entre 100px y 200px
+  star.style.height = randomWidth + "px";
+  star.style.width = randomWidth + "px";
+  let rotation = Math.floor(Math.random() * (140 - 8 + 1)) + 140; // Tamaño aleatorio entre 100px y 200px
+  star.style.transform = "rotate(" + rotation + "deg);";
+  document.querySelector(".estrellas").appendChild(star);
+}
+//left balloons
+let sec = 0.2;
+let ballonHeight = 70;
+if (window.innerWidth > 1000) {
+  ballonHeight = 120;
+}
+for (let i = 0; i < 12; i++) {
+  let balloon = document.createElement("img");
+  balloon.src = "globogris.png";
+  document.querySelector(".infoDiv").appendChild(balloon);
+
+  balloon.style.height = ballonHeight + "px";
+  balloon.style.position = "absolute";
+  balloon.style.filter = "contrast(20%)";
+  balloon.style.filter = "brightness(110%)";
+  balloon.style.bottom = "-120px";
+  balloon.style.animation = " move 6s 1 linear";
+  balloon.style.animationDelay = sec + "s";
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 1000) {
+      balloon.style.height = "100px";
+    } else {
+      balloon.style.height = "70px";
+    }
+  });
+  switch (i % 3) {
+    case 0:
+      balloon.style.left = "0px";
+      break;
+    case 1:
+      balloon.style.left = "30px";
+      break;
+    case 2:
+      balloon.style.left = "60px";
+
+      break;
+  }
+
+  sec += 0.6;
+}
+sec = 0.2;
+for (let i = 0; i < 13; i++) {
+  let balloon = document.createElement("img");
+  balloon.src = "globogris.png";
+  document.querySelector(".infoDiv").appendChild(balloon);
+  balloon.style.height = ballonHeight + "px";
+  balloon.style.position = "absolute";
+  balloon.style.filter = "contrast(20%)";
+  balloon.style.filter = "brightness(120%)";
+  balloon.style.bottom = "-120px";
+  balloon.style.animation = " move 6s 1 linear";
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 1000) {
+      balloon.style.height = "100px";
+    } else {
+      balloon.style.height = "70px";
+    }
+  });
+  balloon.style.animationDelay = sec + "s";
+  switch (i % 3) {
+    case 0:
+      balloon.style.right = "0px";
+      break;
+    case 1:
+      balloon.style.right = "30px";
+      break;
+    case 2:
+      balloon.style.right = "60px";
+
+      break;
+  }
+
+  sec += 0.6;
+}
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+var audio = document.createElement("AUDIO");
+document.body.appendChild(audio);
+audio.src = "cancion.mp3";
+let loader = document.getElementById("loader");
+setTimeout(() => {
+  loader.classList.add("close");
+}, 6000);
+
+
+
+
+
+cant = 35;
+if (window.innerWidth > 800) {
+  cant = 100;
+}
+for (let i = 0; i < cant; i++) {
+  let star = document.createElement("div");
+  star.classList.add("estrella");
+  var randomWidth = Math.floor(Math.random() * (50 - 5 + 1)) + 50; // Tamaño aleatorio entre 100px y 200px
+  star.style.height = randomWidth + "px";
+  star.style.width = randomWidth + "px";
+  let rotation = Math.floor(Math.random() * (140 - 8 + 1)) + 140; // Tamaño aleatorio entre 100px y 200px
+  star.style.transform = "rotate(" + rotation + "deg);";
+  document.querySelector(".estrellas2").appendChild(star);
+}
+let askingWindow = document.querySelector(".popupAsking");
+let estrellas2 = document.querySelector(".estrellas2");
+let a1 = document.getElementById("volume_up")
+let a2 =document.getElementById("volume_off")
+let as = [a1,a2];
+for (let i=0;i<as.length;i++){
+    as[i].onclick = function(){
+        document.body.removeAttribute("class");
+        document.body.classList.add("bodyCustom");
+        askingWindow.removeAttribute("class");
+        estrellas2.removeAttribute("class");
+        askingWindow.classList.add("close");
+        estrellas2.classList.add("close");
+        if(i==0){
+                audio.play();   
+        }
+        return false;
+    }
+}
